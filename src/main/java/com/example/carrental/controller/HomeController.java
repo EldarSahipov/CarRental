8e737dabe61804e22ade5a245bb20306d8225c96
homie652@gmail.com <homie652@gmail.com>
2022-12-19 01:56:00 +0400

    Добавил главную

--- src/main/java/com/example/carrental/controller/HomeController.java
+++ src/main/java/com/example/carrental/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.carrental.controller;
 import com.example.carrental.service.UserService;
 import com.example.carrental.springsecurity.model.Role;
 import com.example.carrental.springsecurity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    private UserService userService;
+    private final UserService userService;
+
+    @Autowired
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public String getHomePage(@AuthenticationPrincipal UserDetails currentUser, Model model) {
